a3a4aa4062f586b12fe8a686b3965c7e9027c271
Luis Alberto Ortega <luisortegar99@gmail.com>
2025-04-08 20:54:26 -0600

    historia del 8 de abril del 2025: se termino el proceso de generar los subtotales apartir de los datos homologados con esto se podra generar el informe de errores de subtotales

--- diiestadistica/procesamiento/procesar_subtotales.py
+++ diiestadistica/procesamiento/procesar_subtotales.py
@@ -67,6 +67,16 @@ def homologar_subtotales(ruta_subtotales,ruta_unidades):
 			print(nombre_archivo)
 
 def sub_totales_unidad(ruta_global):
+	ruta_catalogos = "/home/kaliuser/Documentos/diiestadistica/"
+	ruta_programas = f"{ruta_catalogos}/programas.xlsx"
+	ruta_unidades = f"{ruta_catalogos}/unidades_academicas.xlsx"
+	programas = pd.read_excel(ruta_programas)
+	unidades = pd.read_excel(ruta_unidades)
+	lista_filtro = [col for col in unidades.columns]
+	lista_filtro.append('Programa')
+	lista_filtro.append('Datos')
+	for col in programas.columns:
+		lista_filtro.append(col)
 	ruta_homologado = f"{ruta_global}/archivos_homologados"
 	ruta_subunidades = f"{ruta_global}/subtotales/sub_totales_unidad"
 	for nombre_archivo in os.listdir(ruta_homologado):
@@ -74,39 +84,62 @@ def sub_totales_unidad(ruta_global):
 			if nombre_archivo.endswith('xlsx'):
 				ruta_archivo = f"{ruta_homologado}/{nombre_archivo}"
 				dataframe = pd.read_excel(ruta_archivo)
-				columnas_importantes = [col for col in dataframe.columns if col not in ['Programa','Rama_intranet','Datos']]
-				dataframe = dataframe.groupby(columnas_importantes, as_index=False)['Datos'].sum()
-				write_root = f"{ruta_subunidades}/{nombre_archivo}"
-				dataframe.to_excel(write_root, index=False)
-		except:
-			print(nombre_archivo)
-		
-def sub_totales_rama(ruta_global):
-	ruta_homologado = f"{ruta_global}/archivos_homologados"
-	ruta_subunidades = f"{ruta_global}/subtotales/sub_totales_rama"
-	for nombre_archivo in os.listdir(ruta_homologado):
-		try:
-			if nombre_archivo.endswith('xlsx'):
-				ruta_archivo = f"{ruta_homologado}/{nombre_archivo}"
-				dataframe = pd.read_excel(ruta_archivo)
-				columnas_importantes = [col for col in dataframe.columns if col not in ['Programa','Unidad_Academica','Datos']]
+				columnas_importantes = [col for col in dataframe.columns if col not in lista_filtro]
 				dataframe = dataframe.groupby(columnas_importantes, as_index=False)['Datos'].sum()
 				write_root = f"{ruta_subunidades}/{nombre_archivo}"
 				dataframe.to_excel(write_root, index=False)
 		except:
 			print(nombre_archivo)
 
-def sub_totales_total(ruta_global):
+
+def sub_totales_rama(ruta_global):
+	ruta_catalogos = "/home/kaliuser/Documentos/diiestadistica/"
+	ruta_programas = f"{ruta_catalogos}/programas.xlsx"
+	ruta_unidades = f"{ruta_catalogos}/unidades_academicas.xlsx"
+	programas = pd.read_excel(ruta_programas)
+	unidades = pd.read_excel(ruta_unidades)
+	lista_filtro = [col for col in unidades.columns if col not in ['Rama_intranet']]
+	lista_filtro.append('Programa')
+	lista_filtro.append('Datos')
+	lista_filtro.append('Unidad.Academica')
+	for col in programas.columns:
+		lista_filtro.append(col)
 	ruta_homologado = f"{ruta_global}/archivos_homologados"
-	ruta_subunidades = f"{ruta_global}/subtotales/totales"
+	ruta_sub = f"{ruta_global}/subtotales/sub_totales_rama"
 	for nombre_archivo in os.listdir(ruta_homologado):
 		try:
 			if nombre_archivo.endswith('xlsx'):
 				ruta_archivo = f"{ruta_homologado}/{nombre_archivo}"
 				dataframe = pd.read_excel(ruta_archivo)
-				columnas_importantes = [col for col in dataframe.columns if col not in ['Programa','Rama_intranet','Datos','Unidad_Academica']]
+				columnas_importantes = [col for col in dataframe.columns if col not in lista_filtro]
 				dataframe = dataframe.groupby(columnas_importantes, as_index=False)['Datos'].sum()
-				write_root = f"{ruta_subunidades}/{nombre_archivo}"
+				write_root = f"{ruta_sub}/{nombre_archivo}"
+				dataframe.to_excel(write_root, index=False)
+		except:
+			print(nombre_archivo)
+
+def sub_totales_total(ruta_global):
+	ruta_catalogos = "/home/kaliuser/Documentos/diiestadistica/"
+	ruta_programas = f"{ruta_catalogos}/programas.xlsx"
+	ruta_unidades = f"{ruta_catalogos}/unidades_academicas.xlsx"
+	programas = pd.read_excel(ruta_programas)
+	unidades = pd.read_excel(ruta_unidades)
+	lista_filtro = [col for col in unidades.columns]
+	lista_filtro.append('Programa')
+	lista_filtro.append('Datos')
+	lista_filtro.append('Unidad.Academica')
+	for col in programas.columns:
+		lista_filtro.append(col)
+	ruta_homologado = f"{ruta_global}/archivos_homologados"
+	ruta_sub = f"{ruta_global}/subtotales/totales"
+	for nombre_archivo in os.listdir(ruta_homologado):
+		try:
+			if nombre_archivo.endswith('xlsx'):
+				ruta_archivo = f"{ruta_homologado}/{nombre_archivo}"
+				dataframe = pd.read_excel(ruta_archivo)
+				columnas_importantes = [col for col in dataframe.columns if col not in lista_filtro]
+				dataframe = dataframe.groupby(columnas_importantes, as_index=False)['Datos'].sum()
+				write_root = f"{ruta_sub}/{nombre_archivo}"
 				dataframe.to_excel(write_root, index=False)
 		except:
 			print(nombre_archivo)
@@ -119,7 +152,7 @@ def procesar_subtotales(ruta_global):
     ruta_errores = f"{ruta_global}/errores"
     carpetas=["sub_totales_unidad","sub_totales_rama","totales"]
     eliminar_xlsx_vacios(ruta_subtotales)
-    homologar_subtotales(ruta_subtotales,ruta_programas,ruta_unidades)
+    homologar_subtotales(ruta_subtotales,ruta_unidades)
     crear_subdirectorios(ruta_subtotales, carpetas)
     sub_totales_unidad(ruta_global)
     sub_totales_rama(ruta_global)
--- diiestadistica/tests/proc_subtotales.py
+++ diiestadistica/tests/proc_subtotales.py
@@ -1,8 +1,11 @@
 from ..procesamiento.procesar_subtotales import crear_subdirectorios
 from ..procesamiento.procesar_subtotales import sub_totales_unidad
+from ..procesamiento.procesar_subtotales import sub_totales_rama
+from ..procesamiento.procesar_subtotales import sub_totales_total
+from ..procesamiento.procesar_subtotales import procesar_subtotales
 from ..gui.seleccion_archivos import seleccionar_carpeta
 
 ruta = seleccionar_carpeta()
 #carpetas=["sub_totales_unidad","sub_totales_rama","totales"]
 #crear_subdirectorios(ruta, carpetas)
-sub_totales_unidad(ruta)
+procesar_subtotales(ruta)
